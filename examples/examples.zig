11c916b01d515a7cea3fb21daba236d3476b20d6
NikitaIT <nikita2008101@gmail.com>
2024-07-24 01:47:10 +0200

    If statement expression

--- examples/examples.zig
+++ examples/examples.zig
@@ -47,3 +47,10 @@ test "If Expressions" {
     }
     try assert.expect(x == 1);
 }
+
+test "if statement expression" {
+    const a = true;
+    var x: u16 = 0;
+    x += if (a) 1 else 2;
+    try assert.expect(x == 1);
+}
