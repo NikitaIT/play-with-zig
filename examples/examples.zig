f522994456a66617cc4e3a1e6547b790ac62260e
NikitaIT <nikita2008101@gmail.com>
2024-07-24 02:07:01 +0200

    continue

--- examples/examples.zig
+++ examples/examples.zig
@@ -74,3 +74,13 @@ test "while with continue expression instead of for" {
     }
     try expect(sum == 55);
 }
+
+test "while with continue" {
+    var sum: u8 = 0;
+    var i: u8 = 0;
+    while (i <= 3) : (i += 1) {
+        if (i == 2) continue;
+        sum += i;
+    }
+    try expect(sum == 4);
+}
