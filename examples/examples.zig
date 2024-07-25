dee6a3e450c915023f4d1b71774326681a7a5a78
NikitaIT <nikita2008101@gmail.com>
2024-07-25 22:47:24 +0200

    fibonacci

--- examples/examples.zig
+++ examples/examples.zig
@@ -116,3 +116,13 @@ test "for" {
 
     for (string) |_| {}
 }
+
+fn fibonacci(n: u16) u16 {
+    if (n == 0 or n == 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+test "function recursion" {
+    const x = fibonacci(10);
+    try expect(x == 55);
+}
