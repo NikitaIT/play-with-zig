5c5ad5230e89393d4f594b77a8a6c04227c6a4fe
NikitaIT <nikita2008101@gmail.com>
2024-07-25 23:54:56 +0200

    docs

--- main.zig
+++ main.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const print = std.debug.print;
+const assert = std.debug.assert;
 
 /// docs: zig test -femit-docs main.zig
 /// This is the main function for the program.
@@ -7,5 +9,48 @@ pub fn main() void {
     // The line below is a comment and won't be executed.
 
     //print("Hello?", .{});
-    std.debug.print("Hello, {s}!\n", .{"World"}); // another comment
+    print("Hello, {s}!\n", .{"World"}); // another comment
+    // integers
+    const one_plus_one: i32 = 1 + 1;
+    print("1 + 1 = {}\n", .{one_plus_one});
+
+    // floats
+    const seven_div_three: f32 = 7.0 / 3.0;
+    print("7.0 / 3.0 = {}\n", .{seven_div_three});
+
+    // boolean
+    print("{}\n{}\n{}\n", .{
+        true and false,
+        true or false,
+        !true,
+    });
+
+    // optional
+    var optional_value: ?[]const u8 = null;
+    assert(optional_value == null);
+
+    print("\noptional 1\ntype: {}\nvalue: {?s}\n", .{
+        @TypeOf(optional_value), optional_value,
+    });
+
+    optional_value = "hi";
+    assert(optional_value != null);
+
+    print("\noptional 2\ntype: {}\nvalue: {?s}\n", .{
+        @TypeOf(optional_value), optional_value,
+    });
+
+    // error union
+    var number_or_error: anyerror!i32 = error.ArgNotFound;
+
+    print("\nerror union 1\ntype: {}\nvalue: {!}\n", .{
+        @TypeOf(number_or_error),
+        number_or_error,
+    });
+
+    number_or_error = 1234;
+
+    print("\nerror union 2\ntype: {}\nvalue: {!}\n", .{
+        @TypeOf(number_or_error), number_or_error,
+    });
 }
